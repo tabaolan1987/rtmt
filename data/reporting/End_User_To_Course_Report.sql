ba125e926efdf7f397ffad2d38973396edcf5547
lan.ta <lan.ta@c-mg.com>
2014-01-24 10:31:10 +0700

    update querry report

--- data/reporting/End_User_To_Course_Report.sql
+++ data/reporting/End_User_To_Course_Report.sql
@@ -1,52 +1,53 @@
-Select{
-%
-NTID,Full Name,First Name,Surname,OMS Function
-|
-select udata.ntid,(udata.lname + ',' + udata.fname) as [Fullname],
-udata.fname,udata.lname,udata.omsSubfunction
-from (((((((user_data as udata 
-inner join specialism as sp)
-on sp.SpecialismName = udata.specialism
-inner join SpecialismMappingActivity as spAc
-on sp.id = spAc.idSpecialism)
-inner join Activity as ac
-on spAc.idActivity =ac.idActivity)
-inner join MappingActivityBpStandardRole as AcBpMapp
-on ac.idActivity = AcBpMapp.idActivity)
-inner join BpRoleStandard as bpRole
-on AcBpMapp.idBpRoleStandard = bpRole.id)
-inner join CourseMappingBpRoleStandard as courseMapp
-on courseMapp.idBpRole = bpRole.id)
-inner join course as cou
-on cou.id = courseMapp.idCourse) 
-group by cou.courseId,udata.fname,udata.lname,udata.omsSubfunction,udata.ntid
-%}
-=====
-
-=====
-Select{%
-Course Area,Course ID,Course Name,Course Type,Course Duration,Primary / Secondary,Pre / Post Live
-|
-select cou.courseArena,cou.courseId,
-cou.courseTitle,cou.courseType,cou.courseDuration,
-'' as [Primary / Secondary],cou.courseDelivery
+select rpc.ntid,rpc.Fullname,rpc.fname,rpc.lname,rpc.omsSubfunction,'' as JobRole,rpc.courseArena,rpc.courseId,
+rpc.courseTitle,rpc.courseType,rpc.courseDuration,rpc.ps,rpc.courseDelivery
+ from (select udata.ntid, udata.region,(fname+','+lname) as Fullname,udata.fname,udata.lname,
+udata.omsSubfunction,cou.courseArena,cou.courseId,cou.courseTitle,cou.courseType,
+cou.courseDuration ,cou.courseDelivery,courseMapp.ps, 
+(select count(*) from (select udata.ntid, udata.region,(fname+','+lname) as Fullname,udata.fname,udata.lname,
+udata.omsSubfunction,cou.courseArena,cou.courseId,cou.courseTitle,cou.courseType,
+cou.courseDuration ,cou.courseDelivery,courseMapp.ps
 from (((((((user_data as udata 
 inner join specialism as sp
 on sp.SpecialismName = udata.specialism)
 inner join SpecialismMappingActivity as spAc
 on sp.id = spAc.idSpecialism)
 inner join Activity as ac
-on spAc.idActivity =ac.idActivity)
+on spAc.idActivity =ac.id)
 inner join MappingActivityBpStandardRole as AcBpMapp
-on ac.idActivity = AcBpMapp.idActivity)
+on ac.id = AcBpMapp.idActivity)
 inner join BpRoleStandard as bpRole
 on AcBpMapp.idBpRoleStandard = bpRole.id)
 inner join CourseMappingBpRoleStandard as courseMapp
 on courseMapp.idBpRole = bpRole.id)
 inner join course as cou
-on cou.id = courseMapp.idCourse )
-where udata.ntid = UD.ntid
-group by cou.courseId, cou.courseTitle, cou.courseType,
-cou.courseDuration,cou.courseDelivery,cou.courseArena
-%}
-FROM user_data AS UD ORDER BY UD.ntid
+on cou.id = courseMapp.idCourse)
+group by udata.ntid,cou.courseId, cou.courseTitle, cou.courseType,
+udata.fname,cou.courseDuration,cou.courseDelivery,cou.courseArena,cou.id,
+udata.lname,udata.omsSubfunction,courseMapp.ps,udata.region) as tbl_cached where tbl_cached.ntid = udata.ntid and tbl_cached.courseId=cou.courseId
+) as count_conflict
+from (((((((user_data as udata 
+inner join specialism as sp
+on sp.SpecialismName = udata.specialism)
+inner join SpecialismMappingActivity as spAc
+on sp.id = spAc.idSpecialism)
+inner join Activity as ac
+on spAc.idActivity =ac.id)
+inner join MappingActivityBpStandardRole as AcBpMapp
+on ac.id = AcBpMapp.idActivity)
+inner join BpRoleStandard as bpRole
+on AcBpMapp.idBpRoleStandard = bpRole.id)
+inner join CourseMappingBpRoleStandard as courseMapp
+on courseMapp.idBpRole = bpRole.id)
+inner join course as cou
+on cou.id = courseMapp.idCourse)
+group by udata.ntid,cou.courseId, cou.courseTitle, cou.courseType,
+udata.fname,cou.courseDuration,cou.courseDelivery,cou.courseArena,cou.id,
+udata.lname,udata.omsSubfunction,courseMapp.ps,udata.region) as rpc
+ where (rpc.count_conflict > 1 and rpc.ps = 'P') or rpc.count_conflict =1
+ and rpc.region='GoM'
+
+
+
+
+
+
