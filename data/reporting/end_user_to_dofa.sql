157b9cb53dbc64a36b18a124338b0c42c993bb34
hailucmg <hai.lu@c-mg.com>
2014-05-05 14:04:21 +0700

    Fix issue DURM-323

--- data/reporting/end_user_to_dofa.sql
+++ data/reporting/end_user_to_dofa.sql
@@ -19,6 +19,7 @@ WHERE BpRoleStandard.Dofa_Type = Dofa.[DOA_SRM_Au]
 and user_data_mapping_role.deleted=0
 and user_data_mapping_role.idRegion='(%RG_NAME%)'
 and Dofa.deleted=0
+and Dofa.region='(%RG_NAME%)'
 and BpRoleStandard.deleted=0
 and user_data.deleted=0
 and user_data.Region='(%RG_NAME%)'
@@ -37,7 +38,7 @@ FROM ((user_data INNER JOIN user_data_mapping_role ON user_data.ntid = user_data
 WHERE BpRoleStandard.Dofa_Type='PO' AND Dofa.[DOA_Spend_Limit] Is Null
 and user_data_mapping_role.deleted=0
 and user_data_mapping_role.idRegion='(%RG_NAME%)'
-and (Dofa.deleted=0 or Dofa.deleted is null)
+and ((Dofa.deleted=0 and Dofa.region='(%RG_NAME%)') or Dofa.deleted is null)
 and BpRoleStandard.deleted=0
 and user_data.deleted=0
 and user_data.Region='(%RG_NAME%)'
@@ -56,7 +57,7 @@ FROM ((user_data INNER JOIN user_data_mapping_role ON user_data.ntid = user_data
 WHERE BpRoleStandard.Dofa_Type='IN' AND Dofa.[DOA_Spend_Limit] Is Null
 and user_data_mapping_role.deleted=0
 and user_data_mapping_role.idRegion='(%RG_NAME%)'
-and (Dofa.deleted=0 or Dofa.deleted is null)
+and ((Dofa.deleted=0 and Dofa.region='(%RG_NAME%)') or Dofa.deleted is null)
 and BpRoleStandard.deleted=0
 and user_data.deleted=0
 and user_data.Region='(%RG_NAME%)'
@@ -75,7 +76,7 @@ FROM ((user_data INNER JOIN user_data_mapping_role ON user_data.ntid = user_data
 WHERE BpRoleStandard.Dofa_Type='IP' AND Dofa.[DOA_Spend_Limit] Is Null
 and user_data_mapping_role.deleted=0
 and user_data_mapping_role.idRegion='(%RG_NAME%)'
-and (Dofa.deleted=0 or Dofa.deleted is null)
+and ((Dofa.deleted=0 and Dofa.region='(%RG_NAME%)') or Dofa.deleted is null)
 and BpRoleStandard.deleted=0
 and user_data.deleted=0
 and user_data.Region='(%RG_NAME%)'
@@ -94,7 +95,7 @@ FROM ((user_data INNER JOIN user_data_mapping_role ON user_data.ntid = user_data
 WHERE BpRoleStandard.Dofa_Type='SA' AND Dofa.[DOA_Spend_Limit] Is Null
 and user_data_mapping_role.deleted=0
 and user_data_mapping_role.idRegion='(%RG_NAME%)'
-and (Dofa.deleted=0 or Dofa.deleted is null)
+and ((Dofa.deleted=0 and Dofa.region='(%RG_NAME%)') or Dofa.deleted is null)
 and BpRoleStandard.deleted=0
 and user_data.deleted=0
 and user_data.Region='(%RG_NAME%)'
