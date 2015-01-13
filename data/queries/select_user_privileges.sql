e6039017d619a59173ba9debdbd7a07b029c1bb6
hailucmg <hai.lu@c-mg.com>
2015-01-13 14:20:54 +0700

    Fix issue DURM-365

--- data/queries/select_user_privileges.sql
+++ data/queries/select_user_privileges.sql
@@ -1,9 +1,10 @@
 select p.ntid,r.RegionName,rmt.roleName,p.permission
-from ((privileges as p
+from (((privileges as p
 inner join Region as r
 on p.idRegion = r.id)
 inner join RMT_ROLES as rmt
-on rmt.id = p.idRoleRMT)
+on rmt.id = p.idRoleRMT) inner join user_rmt as u on u.ntid = p.ntid)
 where p.ntid = '(%VALUE%)' and p.deleted=0 and r.deleted=0 and rmt.deleted=0
+	and u.ntid = '(%VALUE%)' and u.deleted=0
 group by p.ntid,r.RegionName,rmt.roleName,p.permission
 order by p.ntid, r.RegionName, rmt.roleName
